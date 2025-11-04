2596fa190f0a69568601e9b17d1b99c7748e7aab
mudimurtala <ibnmuhyideen95@gmail.com>
2025-11-04 09:44:21 +0100

    Add .go file

---
+++ checkpoints/rectperimeter.go
@@ -0,0 +1,51 @@
+// ## rectperimeter
+// ### Instructions
+// Write a function that takes two `int`'s as arguments, representing the length of width and 
+// height of a rectangle and returning the perimeter of the rectangle.
+// - If one of the arguments is negative it should return `-1`.
+// ### Expected function
+// ```go
+// func RectPerimeter(w, h int) int {
+// }
+// ```
+// ### Usage
+// Here is a possible program to test your function:
+// ```go
+// package main
+// import (
+// 	"fmt"
+// )
+// func main() {
+// 	fmt.Println(RectPerimeter(10, 2))
+// 	fmt.Println(RectPerimeter(434343, 898989))
+// 	fmt.Println(RectPerimeter(10, -2))
+// }
+// ```
+// And its output:
+// ```console
+// $ go run .
+// 24$
+// 2666664$
+// -1$
+// $
+// ```
+
+
+package main
+
+import (
+	"fmt"
+)
+
+func RectPerimeter(w, h int) int {
+	if w < 0 || h < 0 {
+		return -1
+	}
+	return 2 *(w + h)
+}
+
+func main() {
+	fmt.Println(RectPerimeter(10, 2))
+	fmt.Println(RectPerimeter(434343, 898989))
+	fmt.Println(RectPerimeter(10, -2))
+}
