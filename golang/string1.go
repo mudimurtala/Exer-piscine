5c74b861d82c3889f4e2c3fc5c97a374359c614a
mudimurtala <ibnmuhyideen95@gmail.com>
2026-03-03 08:32:10 +0100

    Add a file

---
+++ golang/string1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	text := "Hello"
+	fmt.Println(text)
+	fmt.Println(len(text))
+	fmt.Println(text[0])
+	fmt.Printf("%c\n", text[0])
+	fmt.Printf("%c\n", text[1])
+}
